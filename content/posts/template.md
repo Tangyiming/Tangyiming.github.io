dbb76c7a9252b7d8371b0d00a3505102775643f3
tangyiming <backtym@163.com>
2019-10-19 14:56:30 +0800

    first commit

---
+++ content/posts/template.md
@@ -0,0 +1,18 @@
+---
+# 文章标题，设置path时可以用中文
+title: 
+# 手动指定文章url path，不可以含空格等
+path: 
+# 日期
+date: 2019-01-07
+# 要不要在目录列表显示
+published: false
+# tag 不可以写中文
+tags: ['', '']
+series: false
+# 图片封面
+cover_image: ./images/alexandr-podvalny-220262-unsplash.jpg
+canonical_url: false
+# 一句话描述
+description: ""
+---
---
+++ src/components/Author.vue
@@ -0,0 +1,100 @@
+<template>
+	<div class="author">
+
+		<g-image alt="Author image" class="author__image" src="~/assets/images/author.jpg" width="180" height="180" blur="5" />
+
+		<h1 v-if="showTitle" class="author__site-title">
+			{{ $static.metaData.siteName }}
+		</h1>
+
+		<p class="author__intro">
+			 I'm a Performance Engineer worked @Splunk ShangHai
+		</p>
+
+		<p class="author__links">
+			<!-- <a href="//twitter.com/gridsome">Follow on Twitter</a> -->
+
+			<a href="//github.com/Tangyiming">GitHub</a>
+			<a href="/my-resume">Resume</a>
+			<a class="author__links__qrcode" href="javascript:">一门性能课（公众号）</a>
+		</p>
+
+
+
+	</div>
+</template>
+
+<static-query>
+query {
+  metaData {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+export default {
+	props: ['showTitle']
+}
+</script>
+
+<style lang="scss">
+.author {
+	margin: 0 auto;
+	max-width: 500px;
+	text-align: center;
+	padding: calc(var(--space) / 2) 0;
+
+	&__image {
+		border-radius: 100%;
+		width: 90px;
+		height: 90px;
+		margin-bottom: 1em;
+	}
+
+	&__intro {
+		opacity: .8;
+	}
+
+	&__site-title {
+		font-size: 1.5em;
+	}
+
+	&__links {
+		margin-top: -.5em;
+		a {
+			margin: 0 .5em;
+			text-decoration: none;
+			border-bottom: 1px solid #ACC0D8;
+		}
+
+		&__qrcode {
+			position: relative;
+		}
+
+		&__qrcode::after {
+			content: url(../assets/images/qrcode.jpg);
+			position: absolute;
+			right: -68px;
+			top: -265px;
+			z-index: 99;
+			width: 258px;
+			height: 258px;
+			border: 5px solid #0095ba;
+			border-radius: 4px;
+			-webkit-transform-origin: top right;
+			transform-origin: bottom left;
+			-webkit-transform: scale(0);
+			transform: scale(0);
+			opacity: 0;
+			-webkit-transition: all .4s ease-in-out;
+			transition: all .4s ease-in-out;
+		}
+
+		&__qrcode:hover::after {
+			transform: scale(1);
+			opacity: 1;
+		}
+	}
+}
+</style>
